6c3b7a5a088b04eab0aae3c6db6409886eef2064
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2025-10-02 14:15:46 +0000

    aula 002

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
-import Meunome from './componentes/primeiroComponente'
-import Cesta from './src/telas/Cesta';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts,Montserrat_400Regular,Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 
 export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular":Montserrat_400Regular,
+    "MontserratBold":Montserrat_700Bold,
+  });
+
+  if(!fonteCarregada){
+    return(<>
+          </>
+          )
+  }
+
   return (
      <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
